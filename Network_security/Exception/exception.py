590fcbb1efa2f4ba17ba5af97c12297bbf8044e3
dhruvikhasia <khasiadhruvi@gmail.com>
2025-09-17 17:41:37 +0530

    exception Handling and Logging completed

---
+++ Network_security/Exception/exception.py
@@ -0,0 +1,13 @@
+
+
+import sys
+from Network_security.Logging import logger
+class NetworkSecurityException (Exception): 
+    def __init__(self, error_message, error_details:sys):
+         self.error_message = error_message 
+         _,_,exc_tb = error_details.exc_info()
+         self.lineno=exc_tb.tb_lineno
+         self.file_name=exc_tb.tb_frame.f_code.co_filename
+    def __str__(self):
+         return "Error occured in python script name [{0}] line number []"
+         self.file_name, self.lineno, str(self.error_message)
